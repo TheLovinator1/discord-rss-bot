32fd7fddc8cbaf98e350d9f24c21cb425dd95c30
Joakim Hellsén <tlovinator@gmail.com>
2022-12-08 13:01:48 +0100

    Make functions smaller

--- discord_rss_bot/settings.py
+++ discord_rss_bot/settings.py
@@ -3,22 +3,19 @@
 Functions:
     create_settings_file:
         Create the settings file if it doesn't exist.
-    get_data_dir:
-        Path to the data directory. This is where the database file and config file are stored.
     get_db_file:
         Where we store the database file.
     read_settings_file:
         Read the settings file and return it as a dict.
 
 Variables:
-    data_directory:
-        The application directory, defaults to user_data_dir().
+    data_dir:
+        Where we store the database and settings file.
     logger:
         The logger for this program.
 """
 import logging
 import os
-from pathlib import Path
 
 from platformdirs import user_data_dir
 from reader import Reader, make_reader
@@ -29,22 +26,25 @@ from tomlkit.toml_document import TOMLDocument
 logging.basicConfig(level=logging.DEBUG, format="[%(asctime)s] [%(funcName)s:%(lineno)d] %(message)s")
 logger: logging.Logger = logging.getLogger(__name__)
 
-# For get_data_dir()
-data_directory: str = user_data_dir(appname="discord_rss_bot", appauthor="TheLovinator", roaming=True)
+data_dir: str = user_data_dir(appname="discord_rss_bot", appauthor="TheLovinator", roaming=True)
+os.makedirs(data_dir, exist_ok=True)
 
 
-def create_settings_file(settings_file) -> None:
-    """Create the settings file if it doesn't exist."""
-    logger.debug(f"Settings file: {settings_file}")
+def create_settings_file(settings_file_location) -> None:
+    """Create the settings file if it doesn't exist.
 
-    # [webhooks]
-    # Both options are commented out by default.
+    Args:
+        settings_file_location: The location of the settings file.
+
+    Returns:
+        None
+    """
+    logger.debug(f"{settings_file_location=}")
+
     webhooks: Table = table()
     webhooks.add(comment('"First webhook" = "https://discord.com/api/webhooks/1234567890/abcdefghijklmnopqrstuvwxyz"'))
     webhooks.add(comment('"Second webhook" = "https://discord.com/api/webhooks/1234567890/abcdefghijklmnopqrstuvwxyz"'))
 
-    # [database]
-    # Option is commented out by default.
     database: Table = table()
     database.add(comment('"location" = "/path/to/database/file"'))
 
@@ -52,64 +52,45 @@ def create_settings_file(settings_file) -> None:
     doc.add("webhooks", webhooks)
     doc.add("database", database)
 
-    logger.debug(f"Settings file: {doc}")
-    logger.debug(f"Settings file as TOML: {doc.as_string()}")
-
     # Write the settings file
-    with open(settings_file, "w") as f:
+    with open(settings_file_location, "w") as f:
         f.write(doc.as_string())
 
 
-def get_db_file(custom_db_name: str = "db.sqlite") -> Path:
-    """Where we store the database file
+def get_db_location(custom_name: str = "db.sqlite") -> str:
+    """Where we store the database file.
 
     Args:
-        custom_db_name: The name of the database file, defaults to db.sqlite.
+        custom_name: The name of the database file, defaults to db.sqlite.
 
     Returns:
-        Path: The database file.
+        The database location.
     """
-    if custom_db_name != "db.sqlite":
-        logger.info(f"Using custom database file: {custom_db_name}")
+    db_name = os.path.join(data_dir, custom_name)
+    logger.debug(f"{db_name=}{f', with custom db name {custom_name!r}' if custom_name != 'db.sqlite' else ''}")
 
-    # Store the database file in the data directory
-    data_dir = user_data_dir(appname="discord_rss_bot", appauthor="TheLovinator", roaming=True)
-    os.makedirs(data_dir, exist_ok=True)
-
-    db_location: Path = Path(os.path.join(data_dir, custom_db_name))
-
-    logger.debug(f"Database file: {db_location}")
-
-    return Path(db_location)
+    return db_name
 
 
-def read_settings_file(custom_settings_name: str = "settings.toml") -> TOMLDocument:
-    """Read the settings file
+def read_settings_file(custom_name: str = "settings.toml") -> TOMLDocument:
+    """Read the settings file and return the settings as a dict.
 
     Args:
-        custom_settings_name: The name of the settings file, defaults to settings.toml.
+        custom_name: The name of the settings file, defaults to settings.toml.
 
     Returns:
         dict: The settings file as a dict.
     """
-    if custom_settings_name != "settings.toml":
-        logger.info(f"Using custom name for settings file: {custom_settings_name}")
 
-    # Store the database file in the data directory.
-    data_dir = user_data_dir(appname="discord_rss_bot", appauthor="TheLovinator", roaming=True)
-    os.makedirs(data_dir, exist_ok=True)
+    settings_file = os.path.join(data_dir, custom_name)
+    logger.debug(f"{settings_file=}{f', with custom db name {custom_name!r}' if custom_name != 'db.sqlite' else ''}")
 
-    settings_file_location: Path = Path(os.path.join(data_dir, custom_settings_name))
+    with open(settings_file, encoding="utf-8") as f:
+        contents: TOMLDocument = parse(f.read())
+        logger.debug(f"{contents=}")
 
-    # Create the settings file if it doesn't exist
-    if not os.path.exists(settings_file_location):
-        create_settings_file(settings_file_location)
-
-    with open(settings_file_location, encoding="utf-8") as f:
-        data: TOMLDocument = parse(f.read())
-        logger.debug(f"Contents of settings file: {data}")
-
-        return data
+        return contents
 
 
-reader: Reader = make_reader(str(get_db_file()))
+db_location: str = get_db_location()
+reader: Reader = make_reader(db_location)
