c80837f48166fc6cdae8f5d582b280bb265bbb7d
Joakim Hellsén <tlovinator@gmail.com>
2022-12-19 02:08:36 +0100

    Add healthcheck program

---
+++ discord_rss_bot/healthcheck.py
@@ -0,0 +1,20 @@
+"""This is used as a healthcheck in the Dockerfile.
+The normal way would probably be to use CURL, but then we would have to install it."""
+
+import sys
+
+import requests
+
+
+def healthcheck():
+    """Check if the website is up."""
+    try:
+        r = requests.get("http://localhost:5000")
+        if r.ok:
+            sys.exit(1)
+    except requests.exceptions.RequestException:
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    healthcheck()
