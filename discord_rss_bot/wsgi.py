41484f7e61c3f8a8cf82894f80d7b836a49502d3
Joakim Hellsén <tlovinator@gmail.com>
2024-05-01 19:51:00 +0200

    Update Django settings

--- discord_rss_bot/wsgi.py
+++ discord_rss_bot/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for discord_rss_bot project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
 
+from django.core.handlers.wsgi import WSGIHandler
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'discord_rss_bot.settings')
+os.environ.setdefault(key="DJANGO_SETTINGS_MODULE", value="discord_rss_bot.settings")
 
-application = get_wsgi_application()
+application: WSGIHandler = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+
 import os
 import sys
 
 
-def main():
+def main() -> None:
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'discord_rss_bot.settings')
+    os.environ.setdefault(key="DJANGO_SETTINGS_MODULE", value="discord_rss_bot.settings")
     try:
-        from django.core.management import execute_from_command_line
+        from django.core.management import execute_from_command_line  # noqa: PLC0415
     except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+        msg = "Couldn't import Django. Have you run `poetry install` or `poetry shell`?"
+        raise ImportError(msg) from exc
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
