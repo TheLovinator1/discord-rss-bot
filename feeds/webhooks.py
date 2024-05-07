61af0ead9b2fd99a846709296897cf3d186a04a5
Joakim Hellsén <tlovinator@gmail.com>
2024-05-07 02:16:18 +0200

    Update stuff and fix pre-commit things

--- feeds/webhooks.py
+++ feeds/webhooks.py
@@ -3,5 +3,5 @@ from reader import Entry
 
 def send_entry_to_webhook(entry: Entry, webhook_url: str) -> str:
     """Send an entry to a webhook."""
-    # TODO(TheLovinator): Implement this
+    # TODO(TheLovinator): Implement this  # noqa: TD003
     return "Sent entry to webhook"
